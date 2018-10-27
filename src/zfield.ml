0d7ccff00773cdf51d29abafee036535904a0dc4
Markus Rudy <webmaster@burgerdev.de>
2018-10-27 23:36:40 +0200

    use Z.erem for subtraction
    This is much faster than manually adding `p` until
    the remainder becomes positive.

--- src/zfield.ml
+++ src/zfield.ml
@@ -7,18 +7,15 @@ module Zp(P: sig val p: Z.t end): Curve.Field with type t = Z.t = struct
   let zero = Z.zero
   let one = Z.one
 
-  let ( + ) x y = Z.rem (Z.add x y) p
-  let ( * ) x y = Z.rem (Z.mul x y) p
+  let ( + ) x y = Z.erem (Z.add x y) p
+  let ( * ) x y = Z.erem (Z.mul x y) p
 
   let double x = x + x
   let square x = x * x
 
-  let rec negate = function
-    | x when Z.lt x Z.zero -> Z.add x p |> negate
-    | x -> Z.rem (Z.sub p (Z.rem x p)) p
-
   let invert x = Z.invert x p
 
   let ( / ) x y = x * invert y
-  let ( - ) x y = x + negate y
+  let ( - ) x y = Z.(erem (x - y) p)
+
 end
