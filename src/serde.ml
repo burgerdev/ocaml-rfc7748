6f6c0ed83f750149d88bd80f2f8f216290937672
Markus Rudy <webmaster@burgerdev.de>
2018-11-21 23:24:00 +0100

    remove cstruct from serializer

--- src/serde.ml
+++ src/serde.ml
@@ -1,5 +1,3 @@
-let byte_size = Z.of_int 256
-
 let z_of_cstruct cs =
   let iter = Cstruct.iter (fun _ -> Some 1) (fun cs -> Cstruct.get_uint8 cs 0) cs in
 
@@ -11,25 +9,17 @@ let z_of_cstruct cs =
       aux acc shift
   in aux Z.zero 0
 
-let cstruct_of_z size z =
-  let cs = Cstruct.create size in
-  for i = 0 to size - 1 do
-    let v = Z.rem (Z.(asr) z (8*i)) byte_size |> Z.to_int in
-    Cstruct.set_uint8 cs i v
-  done;
-  cs
-
-let hex_of_cstruct cs =
-  let n = Cstruct.len cs in
-  let dst = Bytes.create (2 * n) in
-  for i = 0 to n - 1 do
-    let src = Format.sprintf "%02x" Cstruct.(get_uint8 cs i) in
-    BytesLabels.blit_string ~src ~src_pos:0 ~dst ~dst_pos:(i*2) ~len:2
-  done;
-  (* This is ok because we created the string and will forget it after returning. *)
-  Bytes.unsafe_to_string dst
-
 let cstruct_of_hex = Cstruct.of_hex
 
 let z_of_hex hex = cstruct_of_hex hex |> z_of_cstruct
-let hex_of_z n z = cstruct_of_z n z |> hex_of_cstruct
+
+let hex_of_z n z =
+  let num_hex = 2 * n in
+  let upper_bound = n - 1 in
+  let src = Z.format ("%0" ^ string_of_int num_hex ^ "x") z in
+  let dst = Bytes.create num_hex in
+  for i = 0 to upper_bound do
+    BytesLabels.blit_string ~src ~src_pos:(2*i) ~dst ~dst_pos:(2*(upper_bound-i)) ~len:2
+  done;
+  (* This is ok because we created the string and will forget it after returning. *)
+  Bytes.unsafe_to_string dst
